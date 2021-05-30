cf62f4aa044c56ebd0cdd45acc84c481b8035a05
kuldeepkr16 <kuldeepkr16@gmail.com>
2021-05-30 12:34:31 +0530

    kuber script

---
+++ README.md
@@ -0,0 +1,20 @@
+# Kuber Script
+
+This is an attempt to automate an investment strategy. A script to check your purchased stocks details 
+on daily basis and send email.
+
+# How to use
+
+Pull this repo and then edit my_stocks.csv file and add your purchased stocks details.
+The columns are symbol,avg_price,quantity
+
+symbol = the stock symbol on your account
+
+avg_price = your purchased stock price
+
+quantity = number of stocks
+
+and then edit the script to add to and from emailIDs in global variables.
+
+Run the script using command - 
+python main.py
---
+++ main.py
@@ -0,0 +1,97 @@
+import yfinance as yf
+from datetime import datetime, timedelta
+import pandas as pd
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import logging
+
+logging.basicConfig(level=logging.INFO)
+stocks_details = pd.read_csv('my_stocks.csv')
+DATE = datetime.now() - timedelta(days=3)
+DATE = DATE.strftime("%Y-%m-%d")
+sender = 'emailID'
+receiver = 'emailID'
+
+
+def get_stock_details():
+    """
+    get stocks details from yahoo finance(yfinance) package
+    :return: a list of details dict
+    """
+    final_list_ = list()
+    for i, config in stocks_details.iterrows():
+        symbol = config.get('symbol')
+        avg_price = config.get('avg_price')
+        quantity = config.get('quantity')
+        amount_invested = avg_price * quantity
+        logging.info(f"Checking for stock - {symbol}")
+        try:
+            data = yf.download(f"{symbol}.NS", start=str(DATE))
+        except Exception as e:
+            logging.exception(e)
+            raise e
+        sign = '+'
+        data = data.tail(1)
+        close_price = round(data['Close'][0], 2)
+        amount_now = close_price * quantity
+        nume = close_price
+        deno = avg_price
+        if close_price < avg_price:
+            sign = '-'
+            nume = avg_price
+            deno = close_price
+        percent_diff = ((nume - deno) / deno) * 100
+        percent_diff = round(percent_diff, 2)
+        if sign == '-':
+            percent_diff *= -1
+            profit = 0
+            deductions = 0
+            final_profit = 0
+        else:
+            profit = amount_now - amount_invested
+            # 15% tax + 1.01% broker charges
+            deductions = round((0.1601 * profit), 2)
+            final_profit = round((profit - deductions), 2)
+
+        final_list_.append({"stocks": symbol,
+                            "Average Price": avg_price,
+                            "Current Price": close_price,
+                            "Percentage Change": percent_diff,
+                            "profit": profit, "deductions": deductions,
+                            "final_profit": final_profit})
+    return final_list_
+
+
+def send_mail(html):
+    """
+    send mail to receiver's emailID
+    :param html: final details df to html
+    """
+    logging.info(f"Sending mail to {receiver}")
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "Your stocks details"
+    msg['From'] = sender
+    msg['To'] = receiver
+
+    part = MIMEText(html, 'html')
+    msg.attach(part)
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+
+        server.login(sender, "passcode")
+        server.sendmail(sender, receiver, msg.as_string())
+        logging.info(f'sent mail to the user {receiver}')
+
+
+if __name__ == '__main__':
+    final_list = get_stock_details()
+    df = pd.DataFrame(final_list)
+    df.sort_values(by='Percentage Change', inplace=True, ascending=False)
+    df.reset_index(drop=True, inplace=True)
+    df.index = df.index + 1
+    send_mail(df.to_html())
+    logging.info("Success")
